6a3a4a6b5f0efc8769c986d403f95750a655b7b8
Mark Richardson <markeban@yahoo.com>
2016-05-31 19:27:40 -0700

    made api secure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511035623) do
+ActiveRecord::Schema.define(version: 20160601021706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20160511035623) do
     t.date     "birthdate"
     t.string   "email"
     t.string   "ssn"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "api_key"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
