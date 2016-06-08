b9e53771a11e8f67ebb834032fd32d1dc709f53b
Mark Richardson <markeban@yahoo.com>
2016-06-07 18:19:51 -0700

    added custom angular directive

---
+++ app/assets/javascripts/team_crunchy_directive.js
@@ -0,0 +1,7 @@
+angular.module("app").directive('teamCrunchy', function() {
+  return {
+    restrict: 'E',
+    template: '<h1>Hello Team Crunchy!!!!</h1><br><h2>this is the coolest!</h2>'
+  }
+
+});
